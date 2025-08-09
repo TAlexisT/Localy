5bb454ac1d71aa09db827d38abe281a6cc142de3
Alexis Valles <vallesvictor276@gmail.com>
2025-08-08 22:53:54 -0600

    Primer commit del front de Localy

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-6 rounded-xl shadow-lg">
+      <h1 className="text-3xl font-bold">Localy 🚀</h1>
+      <p className="mt-2">Tailwind está funcionando correctamente</p>
+    </div>
+  );
+}
+
